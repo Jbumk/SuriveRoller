e3ee02e170c5d474ecfa2f905b56524714926243
Jbumk <dhow153@gmail.com>
2022-02-12 01:27:09 +0900

    22-02-12
    멀티모드 삭제
    > 집게컨트롤하는 모드로 변경
    집게 컨트롤 추가

--- Assets/Scripts/FloorControl.cs
+++ Assets/Scripts/FloorControl.cs
@@ -6,9 +6,7 @@ using UnityEngine.EventSystems;
 public class FloorControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [Header("Floor")]
-    private GameObject Floor;
-    public GameObject SingleFloor;
-    public GameObject MultiFloor;  
+    public GameObject Floor;
     public float SpinSpeed=9f;  
     public static Vector3 FloorVec=Vector3.zero;
     private Vector2 AccelSet;
@@ -18,17 +16,9 @@ public class FloorControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
     private float Range = 20f;
     Vector2 inputDir;
 
-    //UI의 vscom 이나 Host로 참가 할때 호출해준다;
-    public void SetSingle()
-    {
-        Floor = SingleFloor;
-    }
+    private bool isball = true;
 
-    public void SetMulti()
-    {
-        Floor = MultiFloor;
-    }
-
+ 
     public void OnBeginDrag(PointerEventData eventData)
     {
 
@@ -72,24 +62,36 @@ public class FloorControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
     private void FixedUpdate()
     {      
         if (GameManager.instance.CountChk() <= 0)
-        {          
-            //컨트롤 방식이 JoyStick(D-pad)방식인지 체크
-            if (UIManager.instance.MethodPadChk())
-            {                
-                FloorVec.x += inputDir.normalized.y * UIManager.instance.XReverseChk();
-                FloorVec.z -= inputDir.normalized.x * UIManager.instance.YReverseChk();
+        {
+            if (isball)
+            {
+                //컨트롤 방식이 JoyStick(D-pad)방식인지 체크
+                if (UIManager.instance.MethodPadChk())
+                {
+                    FloorVec.x += inputDir.normalized.y * UIManager.instance.XReverseChk();
+                    FloorVec.z -= inputDir.normalized.x * UIManager.instance.YReverseChk();
+                }
+                else
+                {
+                    FloorVec.x -= (UIManager.instance.AccelChk().y - Input.acceleration.y) * UIManager.instance.XReverseChk();
+                    FloorVec.z += (UIManager.instance.AccelChk().x - Input.acceleration.x) * UIManager.instance.YReverseChk();
+                }
+                FloorVec.x = Mathf.Clamp(FloorVec.x, -40f, 40f);
+                FloorVec.z = Mathf.Clamp(FloorVec.z, -40f, 40f);
+                Floor.transform.rotation = Quaternion.Euler(FloorVec);
             }
-            else
-            {              
-                FloorVec.x -= (UIManager.instance.AccelChk().y-Input.acceleration.y) * UIManager.instance.XReverseChk();
-                FloorVec.z += (UIManager.instance.AccelChk().x-Input.acceleration.x) * UIManager.instance.YReverseChk();
-            }
-            FloorVec.x = Mathf.Clamp(FloorVec.x, -40f, 40f);
-            FloorVec.z = Mathf.Clamp(FloorVec.z, -40f, 40f);
-            Floor.transform.rotation = Quaternion.Euler(FloorVec);
         }
 
 
     }  
 
+    public void SetBallMode()
+    {
+        isball = true;
+    }
+
+    public void SetPincetMode()
+    {
+        isball = false;
+    }
 }
--- Assets/Scripts/PincetControl.cs
+++ Assets/Scripts/PincetControl.cs
@@ -1,116 +1,221 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PincetControl : MonoBehaviour
+public class PincetControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public GameObject Pincet;
+
+    [Header("Contorl")]
+    public RectTransform Stick;
+    public Image BtnDown;
+    private Vector2 inputDir;
+    private float Range = 20f;
+    private Vector3 PincetVec= Vector3.zero;
+    private Vector3 UpPoint;
+    private Vector3 DownPoint;
+   
+
+
+    [Header("Auto")]
     public GameObject[] MovePoint;
     public GameObject[] GrabPoint;
-    public GameObject Pincet;
+   
     public float MoveSpeed = 10f;
     public float DownSpeed = 10f;
     private int MoveCode =0;
     private bool DownChk = false;
     private bool DoMove = false;
     private Vector3 OriginVec;
-    public bool isMulti=false;
+
+    private bool isBall = true;
 
     //바닥 경고용
     private RaycastHit hit;
     private int laymask =1 << 8;
     private Renderer rend;
 
+
     private void Awake()
     {
         OriginVec = Pincet.transform.position;
+        PincetVec = OriginVec;
         MoveCode = Random.Range(0, MovePoint.Length);
     }
 
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        inputDir = (eventData.position - eventData.pressPosition) / 10;
+        if (inputDir.magnitude < Range)
+        {
+            Stick.anchoredPosition = inputDir;
+        }
+        else
+        {
+            inputDir = inputDir.normalized * Range;
+            Stick.anchoredPosition = inputDir;
+        }
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        inputDir = (eventData.position - eventData.pressPosition) / 10;
+        if (inputDir.magnitude < Range)
+        {
+            Stick.anchoredPosition = inputDir;
+        }
+        else
+        {
+            inputDir = inputDir.normalized * Range;
+            Stick.anchoredPosition = inputDir;
+        }
+
+    }
+     public void OnEndDrag(PointerEventData eventData)
+    {
+        inputDir = Vector2.zero;
+        Stick.anchoredPosition = Vector2.zero;
+        
+    }
+
+
     private void FixedUpdate()
     {
-       
+
         if (GameManager.instance.CountChk() <= 0)
         {
-            switch (isMulti)
+            if (isBall)
             {
-                case true:
-
-                    break;
-
-                    //솔로모드일때 알고리즘
-                case false:
-                    if (DoMove)
+                if (DoMove)
+                {
+                    if (Vector3.Distance(Pincet.transform.position, MovePoint[MoveCode].transform.position) <= 0.1)
                     {
-                        if (Vector3.Distance(Pincet.transform.position, MovePoint[MoveCode].transform.position) <= 0.1)
+                        DoMove = false;
+                        DownChk = true;
+                    }
+                    else
+                    {
+                        Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, MovePoint[MoveCode].transform.position, MoveSpeed * Time.deltaTime);
+                    }
+                }
+                else
+                {
+                    if (DownChk)
+                    {
+
+                        Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, GrabPoint[MoveCode].transform.position, DownSpeed * Time.deltaTime);
+                        if (Physics.Raycast(Pincet.transform.position, Pincet.transform.up * -1, out hit, Mathf.Infinity, laymask))
                         {
-                            DoMove = false;
-                            DownChk = true;
+                            rend = hit.transform.GetComponent<Renderer>();
+                            rend.material.color = Color.red;
                         }
-                        else
+
+                        if (Vector3.Distance(Pincet.transform.position, GrabPoint[MoveCode].transform.position) <= 0.1)
                         {
-                            Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, MovePoint[MoveCode].transform.position, MoveSpeed * Time.deltaTime);
+                            DownChk = false;
                         }
                     }
                     else
                     {
-                        if (DownChk)
+                        Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, MovePoint[MoveCode].transform.position, DownSpeed * Time.deltaTime);
+                        if (Vector3.Distance(Pincet.transform.position, MovePoint[MoveCode].transform.position) <= 0.1)
                         {
-                            Debug.DrawRay(transform.position, transform.up * -30, Color.red);
-                            Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, GrabPoint[MoveCode].transform.position, DownSpeed * Time.deltaTime);
-                            if (Physics.Raycast(transform.position, transform.up * -1, out hit, Mathf.Infinity, laymask))
-                            {
-                                rend = hit.transform.GetComponent<Renderer>();
-                                rend.material.color = Color.red;
-                            }
-
-                            if (Vector3.Distance(Pincet.transform.position, GrabPoint[MoveCode].transform.position) <= 0.1)
-                            {
-                                DownChk = false;
-                            }
+                            MoveCode = Random.Range(0, MovePoint.Length);
+                            DoMove = true;
                         }
-                        else
+                        if (rend != null)
                         {
-                            Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, MovePoint[MoveCode].transform.position, DownSpeed * Time.deltaTime);
-                            if (Vector3.Distance(Pincet.transform.position, MovePoint[MoveCode].transform.position) <= 0.1)
-                            {
-                                MoveCode = Random.Range(0, MovePoint.Length);
-                                DoMove = true;
-                            }
-                            if (rend != null)
-                            {
-                                rend.material.color = Color.white;
-                            }
+                            rend.material.color = Color.white;
+                        }
 
+                    }
+                }
+            }
+            else
+            {
+                if (DownChk)
+                {                                      
+                    Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, DownPoint, DownSpeed * Time.deltaTime);
+                    if (Vector3.Distance(Pincet.transform.position, DownPoint) <= 0.1)
+                    {
+                        DownChk = false;
+                    }
+                }
+                else
+                {
+                    if (DoMove)
+                    {
+                        PincetVec.x += inputDir.normalized.x * Time.deltaTime * MoveSpeed;
+                        PincetVec.z += inputDir.normalized.y * Time.deltaTime * MoveSpeed;
+
+                        PincetVec.x = Mathf.Clamp(PincetVec.x, -6f, 6f);
+                        PincetVec.z = Mathf.Clamp(PincetVec.z, -1f, 11f);
+                        Pincet.transform.position = PincetVec;
+                    }
+                    else
+                    {
+                        Pincet.transform.position = Vector3.MoveTowards(Pincet.transform.position, UpPoint, DownSpeed * Time.deltaTime);
+                        if (Vector3.Distance(Pincet.transform.position, UpPoint) <= 0.1)
+                        {
+                            DoMove = true;
+                            BtnDown.color = Color.green;
                         }
                     }
 
-                    break;
+                    
+                }
             }
 
-
-
-
-
-
-
-
-            
         }
+            
+        
     }
-
-   
 
     public void ResetPincet()
     {
+        PincetVec = OriginVec;
+        Pincet.transform.position = OriginVec;
+        BtnDown.color = Color.green;
         DoMove = true;
         DownChk = false;
         MoveCode = Random.Range(0, MovePoint.Length);
-        Pincet.transform.position = OriginVec;
+       
         if (rend != null)
         {
             rend.material.color = Color.white;
             rend = null;
         }
+    }
+
+    public void PutDown()
+    {
+        if (BtnDown.color != Color.red && GameManager.instance.CountChk() <= 0)
+        {
+            BtnDown.color = Color.red;
+            DownChk = true;
+            DoMove = false;
+            UpPoint = Pincet.transform.position;
+            DownPoint = UpPoint;
+            DownPoint.y = UpPoint.y - 12.5f;
+        }
+    }
+
+    public void SetBallMode()
+    {
+        isBall = true;
+    }
+
+    public void SetPincetMode()
+    {
+        isBall = false;
     }
 
 }
